0d3be54ac48f7f733407471784b0f8ae31f9fb65
Tomek <tomekradecki@gmail.com>
2021-06-18 01:02:29 +0200

    Set up connection to db

---
+++ connect_with_db.py
@@ -0,0 +1,32 @@
+import psycopg2
+
+# connect to the db
+
+
+def connect_to_db():
+    con = psycopg2.connect(
+            host="localhost",
+            database="results",
+            user="robssson",
+            password="robssson123" # password only for this project
+    )
+    return con
+
+
+def insert_row_to_db(con, *values):
+    data = []
+    cur = con.cursor()
+    insert_query = '''insert into results(date, player_1, player_2, tournament_name, player_1_score, player_2_score, 
+                    number_of_sets) VALUES (%s,%s,%s,%s,%s,%s, %s)'''
+    for val in values:
+        data.append(val)
+    data_to_insert = data
+    cur.execute(insert_query, data_to_insert)
+    con.commit()
+    cur.close()
+
+
+def close_connection(con):
+    con.close()
+
+
